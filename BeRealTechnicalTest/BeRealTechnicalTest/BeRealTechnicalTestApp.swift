3f1b72112f1161806e2be899613ee2f24de6e753
Romain <romain.dubreucq@withings.com>
2025-04-26 14:10:30 +0200

    init project

---
+++ BeRealTechnicalTest/BeRealTechnicalTest/BeRealTechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealTechnicalTestApp.swift
+//  BeRealTechnicalTest
+//
+//  Created by Romain Dubreucq on 26/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BeRealTechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeRealTechnicalTest/BeRealTechnicalTestTests/BeRealTechnicalTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealTechnicalTestTests.swift
+//  BeRealTechnicalTestTests
+//
+//  Created by Romain Dubreucq on 26/04/2025.
+//
+
+import Testing
+@testable import BeRealTechnicalTest
+
+struct BeRealTechnicalTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
